51b6d2048165b2954b6e544b61640e92ca314d65
Fredi Rolf Bieging <fredibieging@gmail.com>
2014-02-14 16:25:02 -0200

    Using a list instead of a dictionary to keep memoize the results.

--- 14/14.py
+++ 14/14.py
@@ -1,9 +1,9 @@
 # http://projecteuler.net/problem=14
 
-chain = {1: 1}
+chain = [0, 1]
 
 def generate_sequence(n):
-  if n in chain:
+  if n < len(chain):
     return chain[n]
   if n % 2 == 0:
     n /= 2
@@ -11,7 +11,7 @@ def generate_sequence(n):
     n = 3 * n + 1
   return 1 + generate_sequence(n);
 
-for n in range(1, 1000000):
-  chain[n] = generate_sequence(n)
+for n in range(2, 1000000):
+  chain.append(generate_sequence(n))
 
-print max(chain.keys(), key=lambda x: chain[x])
+print chain.index(max(chain))
