cf3dce21e7750f0ba22dd38168877b07ff6a29cc
Fredi Rolf Bieging <fredi.bieging@trt12.jus.br>
2014-10-06 18:38:01 -0300

    Solved problem 67 - Maximum path sum II. Same code used in problem 18.

---
+++ 67/67.py
@@ -0,0 +1,8 @@
+# http://projecteuler.net/problem=18
+
+triangle = [[0] + [int(number) for number in row.split()] + [0] for row in open("triangle").read().splitlines()]
+
+for i in xrange(1, len(triangle)):
+	for j in xrange(1, len(triangle[i]) - 1):
+		triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1])
+print max(triangle[len(triangle) - 1])
