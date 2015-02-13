c7b8270fdbb73616d06987984f02be57d815b1af
Fredi Rolf Bieging <fredi.bieging@trt12.jus.br>
2015-02-13 15:30:58 -0200

    Small change.

--- 41/41.py
+++ 41/41.py
@@ -3,11 +3,11 @@
 import itertools
 from prime import *
 
-ndigital = "1"
+ndigital = ""
 all_ndigitals = []
 
-for i in xrange(2, 10):
-	all_ndigitals = all_ndigitals + [n for n in list(map("".join, itertools.permutations(ndigital))) if is_prime(int(n))]
+for i in xrange(1, 10):
 	ndigital = ndigital + str(i)
+	all_ndigitals = all_ndigitals + [n for n in list(map("".join, itertools.permutations(ndigital))) if is_prime(int(n))]
 
 print max(all_ndigitals)
