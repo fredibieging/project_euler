42f5ea82ab7fe07ebd188346d5bbfeb229987a06
Fredi Rolf Bieging <fredibieging@gmail.com>
2017-08-21 00:24:40 -0300

    Some improvements.

--- lib/combinatorics.py
+++ lib/combinatorics.py
@@ -1,10 +1,5 @@
 def factorial(n):
-    if n <= 1 and n >= 0:
-        return 1
-    return n * factorial(n - 1)
+    return reduce(lambda x, y: x*y, range(1, n + 1))
 
 def combinatorics(n, r):
-    x = 1
-    for i in range(n, r, -1):
-        x = x * i
-    return x / factorial(n - r)
+    return reduce(lambda x, y: x*y, range(n, r, -1)) / factorial(n - r)
